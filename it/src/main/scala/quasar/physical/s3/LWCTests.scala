83404a811b16af25faa9c80d0f503e6345fc28d6
Edmund Noble <edmundnoble@gmail.com>
2018-05-08 17:41:43 -0400

    Improve errors

--- it/src/main/scala/quasar/physical/s3/LWCTests.scala
+++ it/src/main/scala/quasar/physical/s3/LWCTests.scala
@@ -109,7 +109,8 @@ object LWCTests {
           val recChilds = absoluteChilds.map(Path.refineType).toList.collect {
             case -\/(dir) => dir
           }
-          recChilds.traverseM(findPaths).map(newPair :: _)
+          gather(recChilds.map(findPaths), None)
+            .map(chs => newPair :: chs.join)
       }
 
     def readFiles(paths: Map[ADir, List[APath]]): Task[List[(AFile, Option[List[Data]])]] = {
@@ -146,8 +147,8 @@ object LWCTests {
     }
 
     for {
-      paths <- findPaths(Path.rootDir)
-      pathsMap = paths.toMap
+      paths <- gather(List(findPaths(Path.rootDir)), Some("Find paths"))
+      pathsMap = paths.join.toMap
       files <- readFiles(pathsMap)
     } yield TestFS(files.toMap, pathsMap)
   }
